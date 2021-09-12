1ca8ae3a5073663e032eafa2259b38a2e3991794
Gordon Cheung <gordonhwc1@gmail.com>
2021-09-11 19:29:54 -0700

    Add comments

--- src/components/MultTable.js
+++ src/components/MultTable.js
@@ -6,20 +6,22 @@ class MultTable extends React.Component {
         super(props);
 
         this.state = {
+            //the coordinate of the table cell being clicked on; default is null
             squareCoords: {
                 x: null,
                 y: null
             }
         };
-
+        
         this.handleClick = this.handleClick.bind(this);
     }
 
     handleClick(x, y) {
         if(x === 0 || y === 0) {
+            //returns nothing if x or y equals to 0 (0th row or 0th column) because that's the header of the table
             return;
         } else {
-            //sets the coordinates of the cell
+            //sets the coordinates of the cell based on the table cell selected
             this.setState({
                 squareCoords: {
                     x, y
@@ -28,7 +30,7 @@ class MultTable extends React.Component {
         }
     }
 
-    getSelectedText(x, y) {
+    displayText(x, y) {
         const squareCoords = this.state.squareCoords;
 
         //checks for the coords of the cells and show text accordingly
@@ -38,37 +40,45 @@ class MultTable extends React.Component {
             return y;
         } else if(y === 0) {
             return x;
-        } else if(squareCoords.x === x && squareCoords.y === y) {
+        } 
+        //checks to see if the selected cell is the same as the x & y from the input and then returns the product of x & y
+        else if(squareCoords.x === x && squareCoords.y === y) {
             return x * y;
         } else {
+            //returns nothing if none of the conditions above is met
             return '';
         }
     }
 
     render() {
-        //gets the size of the table from its state
+        //gets the size of the table from the props
         const tableSize = this.props.tableSize;
 
-        //generates an array from 0 to tableSize
+        //generates an array from 0 to tableSize: [0, 1, 2, ..., tableSize]
         const sizeArray = Array.from(Array(tableSize + 1).keys());
 
         return (
             <div className="mult-table">
                 <table>
                     <tbody>{
-                        //generates rows
+                        //generates rows using the .map array iterator
                         sizeArray.map(
                             (y) => {
                                 return(
                                     <tr key={y}>{
-                                        //generates columns
+                                        //generates columns using the .map array iterator
                                         sizeArray.map(
                                             (x) => {
+                                                /*  returns table cells with handleClick assigned to each; 
+                                                    uses the arrow function to avoid 'Maximum updated depth exceeded' error.
+                                                    assign className to different cells depending on the x & y coords;
+                                                    x or y is 0 will be assigned to be a header and the rest is the body.
+                                                */
                                                 return(
                                                     <td key={x} onClick={() => this.handleClick(x, y)} className={(x === 0 || y === 0) ? 'mult-header' : 'mult-body'}>
                                                         {
-                                                            //generates text based on the table cell selected
-                                                            this.getSelectedText(x, y)
+                                                            //generates text based on the table cell
+                                                            this.displayText(x, y)
                                                         }
                                                     </td>
                                                 )
@@ -87,4 +97,5 @@ class MultTable extends React.Component {
     }
 }
 
+//exports the component MultTable
 export default MultTable;
--- src/components/MultTableScreen.js
+++ src/components/MultTableScreen.js
@@ -2,11 +2,14 @@ import React from 'react';
 import MultTable from './MultTable';
 import './MultTableScreen.css';
 
+//this component adds the update function and a title to the multiplication table
 class MultTableScreen extends React.Component {
     constructor(props){
         super(props);
 
+        //the size of the table is a state here which will be passed down to the MultTable component as a prop
         this.state = {
+            //the default is 10
             tableSize: 10
         }
 
@@ -14,35 +17,43 @@ class MultTableScreen extends React.Component {
     }
 
     handleClick() {
+        //prompts the user about the tableSize and converts the response(string, initially) to the number type
         let size = Number(prompt("What size do you want the table to be? (Default is 10): "));
 
+        //checks if the response is empty or non-integer and resets the size to the default is that is true
         if(isNaN(size) || Number.isInteger(size) === false) {
             alert("Please enter an integer!");
             size = 10;
         }
 
+        //checks if the response is smaller or equal to 0 and resets the size to default if true
         if(size <= 0) {
-            size = 10;
             alert("Size has to be above 0!");
+            size = 10;
         }
 
+        //sets the tableSize state to the response
         this.setState({
             tableSize: size
         });
     }
 
     render() {
+        /*
+            Adds a title and an updating button with a click handler to the existing table component
+        */
         return (
             <div className="table-screen">
                 <header>
                     <h1>React Multiplication Table</h1>
                 </header>
                 <button onClick={this.handleClick}>Update Table Size</button>
-                <MultTable tableSize={this.state.tableSize} className="MultTable"/>
+                <MultTable tableSize={this.state.tableSize}/>
                 <p>Made by Gordon Cheung, 2021</p>
             </div>
         )
     }
 }
 
+//exports the component
 export default MultTableScreen;
